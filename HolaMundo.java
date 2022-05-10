a750a6aad1ca41c88cd1222e15250d55d9a2a14a
HenryGalvezQ <hgalvezq@unsa.edu.pe>
2022-05-10 18:00:33 -0500

    Ejercicio 1 HolaMundo completado

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
     System.out.println("Hola Mundo");
+    System.out.println("Soy HenryGalvezQ");
     }
 }
